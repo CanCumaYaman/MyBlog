dbf48a502c75b98d89834694d298f43c52e56534
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-03-02 00:58:16 +0300

    02.03.2021

--- MyBlog/Controllers/ArticlesController.cs
+++ MyBlog/Controllers/ArticlesController.cs
@@ -36,5 +36,56 @@ namespace MyBlog.Controllers
         {
             return View();
         }
+        public IActionResult ORM()
+        {
+            return View();
+        }
+        public IActionResult EntityFramework()
+        {
+            return View();
+        }
+       
+        public IActionResult Elasticsearch()
+        {
+            return View();
+        }
+        public IActionResult API()
+        {
+            return View();
+        }
+        public IActionResult Waterfall()
+        {
+            return View();
+        }
+        public IActionResult Agile()
+        {
+            return View();
+        }
+        public IActionResult WaterfallAgile()
+        {
+            return View();
+
+        }
+        public IActionResult IOT()
+        {
+            return View();
+        }
+        public IActionResult Switch()
+        {
+            return View();
+        }
+        public IActionResult SSH()
+        {
+            return View();
+        }
+        public IActionResult WebService()
+        {
+            return View();
+        }
+        public IActionResult Json()
+        {
+            return View();
+        }
+
     }
 }
